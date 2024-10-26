b3ebc1ff9e04b99878144aa73c99344bfe1eca18
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-10-26 19:09:57 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ for name in files:
 
 #Don't be a thief 
 print("Successfully deployed!")
-print("By MaheshChauhan • DroneBots")
+print("By JAAT BOY  • TARGETALLCOURSE")
 
 if __name__ == "__main__":
     bot.run_until_disconnected()
